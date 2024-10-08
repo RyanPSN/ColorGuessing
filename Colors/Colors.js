efcf090509aca2f1e66bacafc9b38d7e17a646d5
RyanPSN <ryangregoire286@gmail.com>
2024-10-08 10:45:53 -0400

    Added Check

--- Colors/Colors.js
+++ Colors/Colors.js
@@ -1,52 +1,42 @@
-function Colors(color) {
-    return color.color;
-}
 var color = [];
 color.push("red");
 color.push("green");
 color.push("blue");
-var random = Math.floor(Math.random() * color.length);
-var randomColor = color[random];
-var input = document.getElementById("guess");
-var value = input.value;
-var a = null;
-function GetCorrectColor() {
-    if (value == "red" && randomColor == "red") {
-        var correctColor = document.getElementById("color1");
-        correctColor.style.backgroundColor = "red";
-        correctColor.textContent = "Correct Color is Red: ";
+var randomColors = Math.floor(Math.random() * color.length);
+var randomColor = color[randomColors];
+var randomColorGuess = function () {
+    var guess = document.getElementById("guess");
+    var value = guess.value;
+    if (value == randomColor && randomColor == "red") {
+        var color1 = document.getElementById("color1");
+        color1.style.backgroundColor = "red";
+        color1.textContent = "Your Correct Color is Red ";
     }
     else if (value == "green" && randomColor == "green") {
-        var correctColor = document.getElementById("color2");
-        correctColor.style.backgroundColor = "green";
-        correctColor.textContent = "Correct Color Green";
+        var color1 = document.getElementById("color2");
+        color1.style.backgroundColor = "Green";
+        color1.textContent = "Your Correct Color is Green ";
     }
     else if (value == "blue" && randomColor == "blue") {
-        var correctColor = document.getElementById("color3");
-        correctColor.style.backgroundColor = "blue";
-        correctColor.textContent = "Correct Color is: Blue";
+        var color1 = document.getElementById("color3");
+        color1.style.backgroundColor = "blue";
+        color1.textContent = "Your Correct Color is Blue";
     }
-    else if (value != randomColor &&
-        randomColor != "blue" &&
-        randomColor != "green") {
-        var correctColor = document.getElementById("color1");
-        correctColor.style.backgroundColor = "red";
-        correctColor.textContent = "Correct Color Red";
+    else if (value == "red" && randomColor == "red") {
+        var color1 = document.getElementById("color1");
+        color1.style.backgroundColor = "red";
+        color1.textContent = "Your Correct Color is Red";
     }
-    else if (value != randomColor &&
-        randomColor != "green" &&
-        randomColor != "blue") {
-        var correctColor = document.getElementById("color1");
-        correctColor.style.backgroundColor = "red";
-        correctColor.textContent = "Correct Color was: Red";
+    else if (value == "green" && randomColor == "blue") {
+        var color1 = document.getElementById("color1");
+        color1.style.backgroundColor = "blue";
+        color1.textContent = "Your Correct Color is Blue ";
     }
-    else if (value != randomColor &&
-        randomColor != "red" &&
-        randomColor != "blue") {
-        var correctColor = document.getElementById("color2");
-        correctColor.style.backgroundColor = "green";
-        correctColor.textContent = "Correct Color is: Green";
+    else if (value == "green" && randomColor != "blue") {
+        var color1 = document.getElementById("color1");
+        color1.style.backgroundColor = "blue";
+        color1.textContent = "Your Correct Color is Blue ";
     }
-}
+};
 var button = document.getElementById("button");
-button.addEventListener("click", GetCorrectColor);
+button.addEventListener("click", randomColorGuess);
--- Colors/Colors.ts
+++ Colors/Colors.ts
@@ -1,64 +1,41 @@
-type Color = {
-  color?: number;
-};
-
-function Colors(color: Color): any {
-  return color.color;
-}
-
-const color: any = [];
+const color: string[] = [];
 color.push("red");
 color.push("green");
 color.push("blue");
 
-const random = Math.floor(Math.random() * color.length);
-let randomColor = color[random];
+const randomColors = Math.floor(Math.random() * color.length);
+const randomColor = color[randomColors];
 
-const input: any = document.getElementById("guess");
-const value = input.value;
+const randomColorGuess = () => {
+  const guess: any = document.getElementById("guess");
+  const value = guess.value;
 
-let a: any = null;
-
-function GetCorrectColor() {
-  if (value == "red" && randomColor == "red") {
-    const correctColor: any = document.getElementById("color1");
-    correctColor.style.backgroundColor = "red";
-    correctColor.textContent = "Correct Color is Red: ";
+  if (value == randomColor && randomColor == "red") {
+    const color1: any = document.getElementById("color1");
+    color1.style.backgroundColor = "red";
+    color1.textContent = "Your Correct Color is Red ";
   } else if (value == "green" && randomColor == "green") {
-    const correctColor: any = document.getElementById("color2");
-    correctColor.style.backgroundColor = "green";
-    correctColor.textContent = "Correct Color Green";
+    const color1: any = document.getElementById("color2");
+    color1.style.backgroundColor = "Green";
+    color1.textContent = "Your Correct Color is Green ";
   } else if (value == "blue" && randomColor == "blue") {
-    const correctColor: any = document.getElementById("color3");
-    correctColor.style.backgroundColor = "blue";
-    correctColor.textContent = "Correct Color is: Blue";
-  } else if (
-    value != randomColor &&
-    randomColor != "blue" &&
-    randomColor != "green"
-  ) {
-    const correctColor: any = document.getElementById("color1");
-    correctColor.style.backgroundColor = "red";
-    correctColor.textContent = "Correct Color Red";
-  } else if (
-    value != randomColor &&
-    randomColor != "green" &&
-    randomColor != "blue"
-  ) {
-    const correctColor: any = document.getElementById("color1");
-    correctColor.style.backgroundColor = "red";
-    correctColor.textContent = "Correct Color was: Red";
-  } else if (
-    value != randomColor &&
-    randomColor != "red" &&
-    randomColor != "blue"
-  ) {
-    const correctColor: any = document.getElementById("color2");
-    correctColor.style.backgroundColor = "green";
-    correctColor.textContent = "Correct Color is: Green";
+    const color1: any = document.getElementById("color3");
+    color1.style.backgroundColor = "blue";
+    color1.textContent = "Your Correct Color is Blue";
+  } else if (value == "red" && randomColor == "red") {
+    const color1: any = document.getElementById("color1");
+    color1.style.backgroundColor = "red";
+    color1.textContent = "Your Correct Color is Red";
+  } else if (value == "green" && randomColor == "blue") {
+    const color1: any = document.getElementById("color1");
+    color1.style.backgroundColor = "blue";
+    color1.textContent = "Your Correct Color is Blue ";
+  } else if (value == "green" && randomColor != "blue") {
+    const color1: any = document.getElementById("color1");
+    color1.style.backgroundColor = "blue";
+    color1.textContent = "Your Correct Color is Blue ";
   }
-}
+};
 
 const button: any = document.getElementById("button");
-
-button.addEventListener("click", GetCorrectColor);
+button.addEventListener("click", randomColorGuess);
